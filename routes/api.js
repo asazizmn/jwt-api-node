2cb280f269186361816402fc84941a6d3fd4f53c
Aziz <azizsaboor@gmail.com>
2016-09-19 16:05:50 +0800

    creating and returning token upon authentication

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,25 @@
 /**
  * api.js
- * - contains all the api routes
+ * - contains all the api routes '/api/...'
  */
 
 
-// prepare router middleware
+//
+// dependencies
+//
+
 var express = require('express');
 var router = express.Router();
-
 var User = require('./../models/user');
+var app = require('../app');
+var jwt = require('jsonwebtoken');
 
 
-// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
-// TODO: route middleware to verify a token
+//
+// routing
+//
 
-// '/api' route
+// default
 router.get('/', function (req, res) {
     res.json({
         message: "Welcome to the API!"
@@ -22,12 +27,66 @@ router.get('/', function (req, res) {
 });
 
 
-// '/api/users' - return all users
+// return all users
 router.get('/users', function (req, res) {
     User.find(function (err, users) {
         res.json(users);
     });
 });
+
+
+// TODO: route middleware to verify a token
+
+// POST route to authenticate a user
+router.post('/authenticate', function (req, res) {
+
+    var conditions = {
+        name: req.body.name
+    };
+
+    var callback = function (err, user) {
+        var token, expire = {
+            expiresIn: 60*60*24
+        };
+
+        if (err) throw err;
+
+        // authenticate username and password
+        if (user) {
+            if (req.body.password === user.password) {
+
+                // create token
+                token = jwt.sign(user, app.get('signatureKey'), expire);
+
+                // and return it
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                })
+
+            } else {
+                res.json({
+                    success: false,
+                    message: 'Authentication failed. Wrong password.'
+                });
+            }
+        } else {
+            res.json({
+                success: false,
+                message: 'Authentication failed. User not found.'
+            });
+        }
+    };
+
+
+    // use mongoose to find existing user and return token
+    User.findOne(conditions, callback);
+
+
+
+});
+
 
 
 module.exports = router;
